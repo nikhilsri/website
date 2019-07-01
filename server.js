b180e7f57cc53ab565dd0cd927c0930f5c43e946
Nikhil Srivastava <nikhil.srivastava@mtree.co.in>
2019-07-01 13:21:43 +0530

    made changes for heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var port = process.env.PORT || 80
  
 http.createServer(function(request, response) {
 	
-	fs.readFile("/index.html", function(err, data){
+	fs.readFile("index.html", function(err, data){
 	response.writeHead(200, {'Content-Type': 'text/html'});
 	response.write(data);
 	response.end();
